4535effa19e6b1f83cb72c002978eec389adfed8
eddisonchiek <21034014@myrp.edu.sg>
2022-04-27 11:02:58 +0800

    test

---
+++ app/src/main/java/sg/edu/sg/c346/id21034014/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.sg.c346.id21034014.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
